7b56d9ea8e2b328bef370126f83050dbc969806b
Shanic0120 <juazapataca@unal.edu.co>
2022-10-10 10:50:32 -0500

    main

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,49 @@
+package comunicacion;
+
+public class Fabula extends Escrito {
+
+	private String ensenanza;
+	private String interpretacion;
+
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanza = ensenanza;
+		this.interpretacion = interpretacion;
+	}
+
+	@Override
+	int palabrasTotales(int palabrasPagina) {
+		// TODO Auto-generated method stub
+		return palabrasPagina * 1;
+	}
+
+	@Override
+	String interpretacion() {
+		// TODO Auto-generated method stub
+		return getInterpretacion();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.getOrigen() + "\n" + this.getTitulo() + "\n" + this.getAutor() + "\n" + this.getPaginas() + "\n"
+				+ this.ensenanza;
+	}
+
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+
+}
